d3bc8cb31cb7e589b81c09ba9e2a73a79dcc7e13
Joshua Kunst <jbkunst@gmail.com>
2021-08-09 09:42:28 -0400

    Create setup-01-r.sh

---
+++ bash/setup-01-r.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+# Compile R
+sudo apt-get install -y gfortran libreadline6-dev libx11-dev libxt-dev libpng-dev libjpeg-dev libcairo2-dev xvfb libzstd-dev texinfo texlive texlive-fonts-extra screen wget zlib1g-dev libbz2-dev liblzma-dev libpcre2-dev libcurl4-openssl-dev openjdk-11-jdk
+sudo apt-get install -y g++ gfortran libreadline6-dev libx11-dev libxt-dev libpng-dev libjpeg-dev libcairo2-dev xvfb libbz2-dev libzstd-dev liblzma-dev libcurl4-openssl-dev texinfo texlive texlive-fonts-extra screen wget libpcre2-dev zlib1g-dev libbz2-dev liblzma-dev libpcre2-dev libcurl4-openssl-dev openjdk-11-jdk make
+
+cd /usr/local/src
+sudo wget https://cran.rstudio.com/src/base/R-4/R-4.0.3.tar.gz
+sudo su
+tar zxvf R-4.0.3.tar.gz
+cd R-4.0.3
+./configure --enable-R-shlib
+make
+make install
+cd ..
+rm -rf R-4.0.3*
+exit
+cd
+apt install r-base-core
